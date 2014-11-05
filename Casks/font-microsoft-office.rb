5df24329cd63190c55d5fcd1530fb8bf4dda8414
Colin Dean <git@cad.cx>
2014-11-05 13:30:46 -0500

    Initial commit with Microsoft Office fonts

---
+++ Casks/font-microsoft-office.rb
@@ -0,0 +1,45 @@
+class FontMicrosoftOffice < Cask
+  version '1.000'
+  sha256 '6833e3fe8aa82b8aa675a654fc3ed496c3faab79579103fab8f4ff94a1be0fd1'
+
+  url 'http://www.rmtweb.co.uk/wp-content/uploads/2013/06/Calibri-and-Cambria-Fonts.zip'
+  homepage 'http://www.rmtweb.co.uk/calibri-and-cambria-fonts-for-mac'
+  license :commercial
+
+  caveats <<-CAVS.gsub(/^[\s]*/, '').strip
+    These fonts are copyrighted by Microsoft.
+
+    Microsoft UK confirmed you are allowed to have them,
+    provided that you own a copy of either:
+
+    * Microsoft PowerPoint Viewer (free)
+    * Microsoft Office (Any version, Windows or Mac)
+
+    For more information, see #{homepage}.
+    CAVS
+
+  font 'Calibri and Cambria Fonts/Calibri Bold Italic.ttf'
+  font 'Calibri and Cambria Fonts/Calibri Bold.ttf'
+  font 'Calibri and Cambria Fonts/Calibri Italic.ttf'
+  font 'Calibri and Cambria Fonts/Calibri.ttf'
+  font 'Calibri and Cambria Fonts/Cambria Bold Italic.ttf'
+  font 'Calibri and Cambria Fonts/Cambria Bold.ttf'
+  font 'Calibri and Cambria Fonts/Cambria Italic.ttf'
+  font 'Calibri and Cambria Fonts/Cambria.ttf'
+  font 'Calibri and Cambria Fonts/Candara Bold Italic.ttf'
+  font 'Calibri and Cambria Fonts/Candara Bold.ttf'
+  font 'Calibri and Cambria Fonts/Candara Italic.ttf'
+  font 'Calibri and Cambria Fonts/Candara.ttf'
+  font 'Calibri and Cambria Fonts/Consolas Bold Italic.ttf'
+  font 'Calibri and Cambria Fonts/Consolas Bold.ttf'
+  font 'Calibri and Cambria Fonts/Consolas Italic.ttf'
+  font 'Calibri and Cambria Fonts/Consolas.ttf'
+  font 'Calibri and Cambria Fonts/Constantia Bold Italic.ttf'
+  font 'Calibri and Cambria Fonts/Constantia Bold.ttf'
+  font 'Calibri and Cambria Fonts/Constantia Italic.ttf'
+  font 'Calibri and Cambria Fonts/Constantia.ttf'
+  font 'Calibri and Cambria Fonts/Corbel Bold Italic.ttf'
+  font 'Calibri and Cambria Fonts/Corbel Bold.ttf'
+  font 'Calibri and Cambria Fonts/Corbel Italic.ttf'
+  font 'Calibri and Cambria Fonts/Corbel.ttf'
+end
---
+++ README.md
@@ -0,0 +1,29 @@
+# Non-Free Fonts
+
+This [Homebrew](http://brew.sh)/[Caskroom](http://caskroom.io) tap contains
+various non-free fonts from around the Internet.
+
+## License and copyright
+
+Note that no font files or archives are kept in this repository. This repository
+simply makes it easier to install the font files from well-known locations.
+
+Assume that each font is copyrighted, but permitted to be used provided that you
+have installed a certain program or otherwise paid a certain party for its use.
+
+If you use a commercial font in a commercial work and you didn't pay for
+the font or otherwise gain permission to use it, the ghost of
+[Morris Fuller Benton](https://en.wikipedia.org/wiki/Morris_Fuller_Benton) will
+mess with your kernings now and forevermore.
+
+## Notable inclusions
+
+* The Microsoft Office 2007 fonts in `font-microsoft-office`
+  * Users of the free Powerpoint Viewer or owners of Microsoft Office can use.
+  * Included fonts:
+    * Calibri
+    * Cambria
+    * Candara
+    * Consolas
+    * Constantia
+    * Corbel
