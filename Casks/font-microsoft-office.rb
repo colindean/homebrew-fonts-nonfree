fab761a33a42089ed427df2dc5cbfa5e2de3ee36
Matr1x-101 <70490301+Matr1x-101@users.noreply.github.com>
2022-01-23 15:50:04 +0000

    Update URLs

--- Casks/font-microsoft-office.rb
+++ Casks/font-microsoft-office.rb
@@ -3,8 +3,8 @@ cask 'font-microsoft-office' do
   version '1.000'
   sha256 '6833e3fe8aa82b8aa675a654fc3ed496c3faab79579103fab8f4ff94a1be0fd1'
 
-  url 'https://www.rmtweb.co.uk/wp-content/uploads/2013/06/Calibri-and-Cambria-Fonts.zip'
-  homepage 'https://www.rmtweb.co.uk/calibri-and-cambria-fonts-for-mac'
+  url 'https://web.archive.org/web/20150226094332/https://www.rmtweb.co.uk/wp-content/uploads/2013/06/Calibri-and-Cambria-Fonts.zip'
+  homepage 'https://web.archive.org/web/20160501234720/http://www.rmtweb.co.uk/calibri-and-cambria-fonts-for-mac'
 
   caveats <<-CAVS.gsub(/^[\s]*/, '').strip
     These fonts are copyrighted by Microsoft.
